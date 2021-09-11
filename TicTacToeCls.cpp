2070de94c9fbcc2846591a2e884fd826e992e506
Şafak Batuhan Ünver <sbunver@hotmail.com>
2021-09-11 17:13:18 +0300

    Add push method and error types for push

--- TicTacToeCls.cpp
+++ TicTacToeCls.cpp
@@ -30,3 +30,28 @@ std::list<std::list<char>> TicTacToe::getGameBoard() const
     return gameBoard;
 }
 
+bool TicTacToe::isMarkValid(char mark)
+{
+    return (mark == 'X') || (mark == 'O');
+}
+
+bool TicTacToe::isPosValid(unsigned int posX, unsigned int posY)
+{
+    return (posX > gameSize) || (posY > gameSize);
+}
+
+TIC_TAC_TOE_RET TicTacToe::push(char mark, unsigned int posX, unsigned int posY)
+{
+    if(!isMarkValid(mark))
+    {
+        return TIC_TAC_TOE_RET_INVALID_MARK;
+    }
+    
+    if(!isPosValid(posX, posY))
+    {
+        return TIC_TAC_TOE_RET_OUT_OF_RANGE_POS;
+    }
+    
+    return TIC_TAC_TOE_RET_PUSH_SUCCESS;
+}
+
--- TicTacToeCls.hpp
+++ TicTacToeCls.hpp
@@ -10,15 +10,27 @@
 
 #include <list>
 
+enum TIC_TAC_TOE_RET
+{
+    TIC_TAC_TOE_RET_OVERWRITE,
+    TIC_TAC_TOE_RET_PUSH_SUCCESS,
+    TIC_TAC_TOE_RET_INVALID_MARK,
+    TIC_TAC_TOE_RET_OUT_OF_RANGE_POS
+};
+
 class TicTacToe
 {
 private:
     std::list<std::list<char>> gameBoard;
     unsigned int gameSize;
     void initWithEmptyMarks();
+    bool isMarkValid(char mark);
+    bool isPosValid(unsigned int posX, unsigned int posY);
+    bool isPosOverwriteable(unsigned int posX, unsigned int posY);
 public:
     TicTacToe(unsigned int size);
     std::list<std::list<char>> getGameBoard() const;
+    TIC_TAC_TOE_RET push(char mark, unsigned int posX, unsigned int posY);
 };
 
 #endif /* TicTacToeCls_hpp */
