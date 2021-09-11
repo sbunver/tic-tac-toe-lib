e9511d58d263404bec245bc5f4b26dc9e187b35b
Şafak Batuhan Ünver <sbunver@hotmail.com>
2021-09-11 15:08:05 +0300

    Add tic-tac-toe class files init version

---
+++ TicTacToeCls.cpp
@@ -0,0 +1,8 @@
+//
+//  TicTacToeCls.cpp
+//  tic-tac-toe-lib
+//
+//  Created by Şafak Batuhan Ünver on 11.09.2021.
+//
+
+#include "TicTacToeCls.hpp"
---
+++ TicTacToeCls.hpp
@@ -0,0 +1,13 @@
+//
+//  TicTacToeCls.hpp
+//  tic-tac-toe-lib
+//
+//  Created by Şafak Batuhan Ünver on 11.09.2021.
+//
+
+#ifndef TicTacToeCls_hpp
+#define TicTacToeCls_hpp
+
+#include <stdio.h>
+
+#endif /* TicTacToeCls_hpp */
